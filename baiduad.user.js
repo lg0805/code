93d096120b288206c95b0d1d19008a23d2707cad
lg0805 <30360993+lg0805@users.noreply.github.com>
2019-11-03 17:04:26 +0800

    Update baiduad.user.js

--- baiduad.user.js
+++ baiduad.user.js
@@ -1,9 +1,18 @@
+// ==UserScript==
+// @name 关闭edu.51cto.com弹窗
+// @namespace Violentmonkey Scripts
+// @author       lg0805
+// @match        *://edu.51cto.com/*
+// @grant none
+// ==/UserScript==
+
 (function(){
-	aDiv = document.getElementsByTagName("div");
-	for(var i=0; i<aDiv.length; i++){
-		if(aDiv[i].style.display && aDiv[i].style.visibility){
-			
-			aDiv[i].style.display = 'none';
+	aP = document.getElementsByTagName('div');
+	
+	for(var i=0; i<aP.length; i++){
+		
+		if(!aP[i].className.indexOf("ins") || aP[i].className=='ins'|| aP[i].className=='upopbg' || aP[i].className=="upopbox_bot" || aP[i].className=="upopbox_mid"){
+			aP[i].style.display='none';
 		}
 	}
-})()
+})();
